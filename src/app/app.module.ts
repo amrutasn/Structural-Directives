d16987465ba2fc4a04796a81eed2a06bb627a6ac
Aditya Kalamkar <adityakalamkar>
2022-03-31 17:21:44 +0530

    new file added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NgIfDemoComponent } from './ng-if-demo/ng-if-demo.component';
 import { NgIfElseComponent } from './ng-if-else/ng-if-else.component';
 import { NgForDemoComponent } from './ng-for-demo/ng-for-demo.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgIfDemoComponent,
     NgIfElseComponent,
-    NgForDemoComponent
+    NgForDemoComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
